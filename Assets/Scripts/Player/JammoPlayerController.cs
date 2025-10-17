4a50f2f85b42783b13ccab385cd8f445342e400a
Bryce <bryce.chg1128@gmail.com>
2025-10-16 22:24:35 -0700

    Added player character movement with animations
    Gave player movement and animation for idle, walk, and run. Player turns towards the correct direction when moving. Also gave the player gravity so it falls after climbing stairs or a ledge.

--- Assets/Scripts/Player/JammoPlayerController.cs
+++ Assets/Scripts/Player/JammoPlayerController.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,16 +11,88 @@ public class JammoPlayerController : MonoBehaviour
     Vector3 moveValue;
     Vector2 moveValueInput;
 
+    private int isWalkingHash;
+    private int isRunningHash;
+
+    private float speed = 2f;
+
+    [Header("Player Attributes")]
+    public float walkSpeed = 2f;
+    public float runSpeed = 4f;
+    public float rotationRate = 15.0f;
+    public float playerGravity = -9.8f;
+
     CharacterController characterController;
+    Animator animator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+
+        isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
 
         moveAction = InputSystem.actions.FindAction("Player/Move");
         jumpAction = InputSystem.actions.FindAction("Player/Jump");
         sprintAction = InputSystem.actions.FindAction("Player/Sprint");
+    }
+
+    void HandleAnimation()
+    {
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool isRunning = animator.GetBool(isRunningHash);
+
+        if (moveAction.IsPressed() && !isWalking)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else if (!moveAction.IsPressed() && isWalking)
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        if ((moveAction.IsPressed() && sprintAction.IsPressed()) && !isRunning)
+        {
+            animator.SetBool("isRunning", true);
+            speed = runSpeed;
+        }
+        else if ((!moveAction.IsPressed() || !sprintAction.IsPressed()) && isRunning)
+        {
+            animator.SetBool("isRunning", false);
+            speed = walkSpeed;
+        }
+    }
+
+    void HandleRotation()
+    {
+        Vector3 rotationPosition;
+
+        rotationPosition.x = moveValue.x;
+        rotationPosition.y = 0.0f;
+        rotationPosition.z = moveValue.z;
+
+        Quaternion currentRotation = transform.rotation;
+
+        if (moveAction.IsPressed())
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(rotationPosition);
+            transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, rotationRate * Time.deltaTime);
+        }
+    }
+
+    void HandleGravity()
+    {
+        if (characterController.isGrounded)
+        {
+            float groundedGravity = -0.05f;
+            moveValue.y = groundedGravity;
+        }
+        else
+        {
+            moveValue.y = playerGravity;
+        }
     }
 
     // Update is called once per frame
@@ -30,6 +103,9 @@ public class JammoPlayerController : MonoBehaviour
         moveValue.x = moveValueInput.x;
         moveValue.z = moveValueInput.y;
 
-        characterController.Move(moveValue * Time.deltaTime);
+        HandleGravity();
+        HandleRotation();
+        HandleAnimation();
+        characterController.Move(moveValue * speed * Time.deltaTime);
     }
 }
