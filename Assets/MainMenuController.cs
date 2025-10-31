dd696fa28bf6c7081901bfe5a0503d9957d53ae4
Bryce <bryce.chg1128@gmail.com>
2025-10-30 22:57:06 -0700

    Added Main Menu

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        SceneManager.LoadScene("MainScene");
+        Time.timeScale = 1f;
+
+    }
+}
